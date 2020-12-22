8c0b3bfba86fb4a31d0f14ec82c23f336c308070
Roger Crawfis <crawfis.3@osu.edu>
2020-12-22 09:26:03 -0500

    Adding GTMY.Utility for now. Might be able to add this as a dependent project in the GTMY.Audio.asmdef file later.

---
+++ Runtime/Shuffle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace GTMY.Utility
+{
+    /// <summary>
+    /// Static class with methods to produce a permutation.
+    /// </summary>
+    public static class Shuffle
+    {
+        /// <summary>
+        /// Create a permuation of the integers from 0 to N-1.
+        /// </summary>
+        /// <param name="length">The size of the permutation.</param>
+        /// <param name="random">A System.Random instance.</param>
+        /// <returns>A list of integers containing the set of numbers from zero to N-1 in a random order.</returns>
+        public static IList<int> CreateRandomPermutation(int length, System.Random random)
+        {
+            // This is the "inside-out" algorithm of Fisher and Yates' shuffle algorithm (Knuth Algorithm P)
+            // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+            int[] permutation = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(i + 1);
+                if (index != i)
+                    permutation[i] = permutation[index];
+                permutation[index] = i;
+            }
+            return permutation;
+        }
+    }
+}
